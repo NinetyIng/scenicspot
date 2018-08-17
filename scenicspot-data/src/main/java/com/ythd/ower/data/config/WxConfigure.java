2d99d073fc87b146d3e66463fd95460d767d3e07
liujunbo <liujunbo@iboxpay.com>
2018-08-17 17:18:00 +0800

    增加微信

---
+++ scenicspot-data/src/main/java/com/ythd/ower/data/config/WxConfigure.java
@@ -0,0 +1,13 @@
+package com.ythd.ower.data.config;
+
+/**
+ * The class  ${CLASS_NAME}.
+ *
+ * Description:
+ *
+ * @author: liujunbo
+ * @since: 2018/8/16
+ * @version: $Revision$
+ */
+public class WxConfigure {
+}
